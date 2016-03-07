ddd38a608b4c6d0a2bb00af628bd711f4a28fef2
jaakko <jaakko>
2016-03-07 19:51:00 +0100

    second chat activity added

---
+++ app/src/main/java/com/example/jk/amqpandroid/MainActivity.java
@@ -0,0 +1,230 @@
+package com.example.jk.amqpandroid;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RatingBar;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        setupConnectionFactory();
+        publishToAMQPRating();
+        setupRatingBar();
+
+          Handler incomingRatingMessageHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+
+                float message = msg.getData().getFloat("rating");
+
+                final RatingBar rb = (RatingBar) findViewById(R.id.ratingBar);
+                if (rb.getRating() != message ) {
+                    rb.setRating(message);
+                    Log.d("'", "RatingBar changed!");
+                }
+            }
+        };
+
+        subscribeRating(incomingRatingMessageHandler);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //MenuInflater inflater = getMenuInflater();
+        //inflater.inflate(R.menu.game_menu, menu);
+
+        menu.add(0,0,0,"Main").setIntent(new Intent(this, MainActivity.class));
+        menu.add(1,1,1,"Second").setIntent(new Intent(this, SecondActivity.class));
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        Log.d("'", "select!"+item.getItemId());
+
+        switch (item.getItemId()) {
+            case 0:
+                Log.d("'", "select! 0");
+                startActivity(item.getIntent());
+                return true;
+            case 1:
+                Log.d("'", "select! 1");
+                startActivity(item.getIntent());
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    void setupRatingBar() {
+        final RatingBar rb = (RatingBar) findViewById(R.id.ratingBar);
+
+        //Initial rating
+        //rb.setRating((float) 2.5);
+
+        rb.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                float rbvalue = rating;
+                Log.d("'", "rbvalue " + rbvalue);
+                Log.d("'", "fromUser "+fromUser);
+                //insert value into internal queue if fromUser
+                if(fromUser) {
+                    publishRatingMessage(rbvalue);
+                    Log.d("'", "onRatingChanged");
+                }
+            }
+        });
+    }
+
+    Thread subscribeThread;
+    Thread publishThread;
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        publishThread.interrupt();
+        subscribeThread.interrupt();
+    }
+
+    int queuedepth = 1;
+    private BlockingDeque<Float> queueRating = new LinkedBlockingDeque(queuedepth);
+    void publishRatingMessage(Float message) {
+        //Adds a message to internal blocking queue
+        try {
+            Log.d("'", "[qRating] " + message);
+            queueRating.putLast(message);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    ConnectionFactory factory = new ConnectionFactory();
+    private void setupConnectionFactory() {
+        String uri = getString(R.string.amqpuri);
+        try {
+            factory.setAutomaticRecoveryEnabled(false);
+            factory.setUri(uri);
+        } catch (KeyManagementException | NoSuchAlgorithmException | URISyntaxException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    void subscribeRating(final Handler handler)
+    {
+        subscribeThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        Connection connection = factory.newConnection();
+                        Channel channel = connection.createChannel();
+                        channel.basicQos(1);
+                        AMQP.Queue.DeclareOk q = channel.queueDeclare();
+                        channel.queueBind(q.getQueue(), "amq.fanout", "rating");
+                        QueueingConsumer consumer = new QueueingConsumer(channel);
+                        boolean autoAck = false;
+                        channel.basicConsume(q.getQueue(), autoAck, consumer);
+
+                        // Process deliveries
+                        while (true) {
+                            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+
+                            ByteBuffer buf = ByteBuffer.wrap(delivery.getBody());
+                            float message = buf.getFloat();
+
+                            Message msg = handler.obtainMessage();
+                            Bundle bundle = new Bundle();
+
+                            bundle.putFloat("rating", message);
+                            msg.setData(bundle);
+                            handler.sendMessage(msg);
+                            boolean requeue = false;
+                            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), requeue );
+                            Log.d("'", "Consumed message!");
+                        }
+                    } catch (InterruptedException e) {
+                        break;
+                    } catch (Exception e1) {
+                        Log.d("'", "Connection broken: " + e1.getClass().getName());
+                        try {
+                            Thread.sleep(4000); //sleep and then try again
+                        } catch (InterruptedException e) {
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+        subscribeThread.start();
+    }
+
+    public void publishToAMQPRating()
+    {
+        publishThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        Connection connection = factory.newConnection();
+                        Channel ch = connection.createChannel();
+                        ch.confirmSelect();
+
+                        while (true) {
+                            int quesize = queueRating.size();
+                            Log.d("'", "QueueSize: "+quesize);
+                            Float message = queueRating.takeFirst();
+                            try{
+
+                                //Allocate space for one float (ch.basicPublish only likes ByteArrays)
+                                final ByteBuffer buf = ByteBuffer.allocate(4).putFloat(message);
+
+                                ch.basicPublish("amq.fanout", "rating", null, buf.array());
+                                Log.d("'", "[s]rating " + message);
+
+                                ch.waitForConfirmsOrDie();
+                            } catch (Exception e){
+                                Log.d("'","[f]rating " + message);
+                                queueRating.putFirst(message);
+                                throw e;
+                            }
+                        }
+                    } catch (InterruptedException e) {
+                        break;
+                    } catch (Exception e) {
+                        Log.d("'", "Connection broken: " + e.getClass().getName());
+                        try {
+                            Thread.sleep(5000); //sleep and then try again
+                        } catch (InterruptedException e1) {
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+        publishThread.start();
+    }
+}
--- app/src/main/java/com/example/jk/amqpandroid/SecondActivity.java
+++ app/src/main/java/com/example/jk/amqpandroid/SecondActivity.java
@@ -1,81 +1,124 @@
 package com.example.jk.amqpandroid;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RatingBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.BasicProperties;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.QueueingConsumer;
 
 import java.net.URISyntaxException;
-import java.nio.ByteBuffer;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Map;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
 
-public class MainPage extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
+        setContentView(R.layout.activity_second);
 
         setupConnectionFactory();
-        publishToAMQPRating();
+        publishToAMQP();
+        setupPubButton();
+        setupDummyPubButton();
 
-        setupRatingBar();
-
-        final Handler incomingRatingMessageHandler = new Handler() {
+        final Handler incomingMessageHandler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
+                String message = msg.getData().getString("msg");
+                TextView tv = (TextView) findViewById(R.id.textView);
+                Date now = new Date();
+                SimpleDateFormat ft = new SimpleDateFormat ("hh:mm:ss");
+                tv.append(ft.format(now) + ' ' + message + '\n');
 
-                float message = msg.getData().getFloat("rating");
+                //Update scrollView to show bottom
+                final ScrollView sc = (ScrollView) findViewById(R.id.scrollView);
+                sc.post(new Runnable()
+                {
+                    public void run()
+                    {
+                        sc.fullScroll(View.FOCUS_DOWN);
+                    }
+                });
 
-                final RatingBar rb = (RatingBar) findViewById(R.id.ratingBar);
-                if (rb.getRating() != message ) {
-                    rb.setRating(message);
-                    Log.d("'", "RatingBar changed!");
-                }
             }
         };
-
-        subscribeRating(incomingRatingMessageHandler);
+        subscribe(incomingMessageHandler);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //MenuInflater inflater = getMenuInflater();
+        //inflater.inflate(R.menu.game_menu, menu);
 
+        menu.add(0,0,0,"Main").setIntent(new Intent(this, MainActivity.class));
+        menu.add(1,1,1,"Second").setIntent(new Intent(this, SecondActivity.class));
 
-    void setupRatingBar() {
-        final RatingBar rb = (RatingBar) findViewById(R.id.ratingBar);
+        return true;
+    }
 
-        //Initial rating
-        //rb.setRating((float) 2.5);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        Log.d("'", "select!" + item.getItemId());
 
-        rb.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+        switch (item.getItemId()) {
+            case 0:
+                Log.d("'", "select! 0");
+                startActivity(item.getIntent());
+                return true;
+            case 1:
+                Log.d("'", "select! 1");
+                startActivity(item.getIntent());
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    void setupPubButton() {
+        Button button = (Button) findViewById(R.id.publish);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                float rbvalue = rating;
-                Log.d("'", "rbvalue " + rbvalue);
-                Log.d("'", "fromUser "+fromUser);
-                //insert value into internal queue if fromUser
-                if(fromUser) {
-                    publishRatingMessage(rbvalue);
-                    Log.d("'", "onRatingChanged");
+            public void onClick(View arg0) {
+                EditText et = (EditText) findViewById(R.id.text);
+                publishMessage(et.getText().toString());
+                Log.d("'", et.getText().toString());
+                et.setText("");
+            }
+        });
+    }
+
+    void setupDummyPubButton() {
+        Button button = (Button) findViewById(R.id.buttondummymsg);
+        button.setOnClickListener(new View.OnClickListener() {
+            int counter = 0;
+            @Override
+            public void onClick(View arg0) {
+                while (counter < 100) {
+                    String msg = "Dummy message " + counter;
+                    counter++;
+                    publishMessage(msg);
+                    Log.d("'", msg);
                 }
+                counter = 0;
             }
         });
     }
@@ -89,13 +132,12 @@ public class MainPage extends AppCompatActivity {
         subscribeThread.interrupt();
     }
 
-    int queuedepth = 1;
-    private BlockingDeque<Float> queueRating = new LinkedBlockingDeque(queuedepth);
-    void publishRatingMessage(Float message) {
+    private BlockingDeque<String> queue = new LinkedBlockingDeque<String>();
+    void publishMessage(String message) {
         //Adds a message to internal blocking queue
         try {
-            Log.d("'", "[qRating] " + message);
-            queueRating.putLast(message);
+            Log.d("'", "[q] " + message);
+            queue.putLast(message);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -112,7 +154,7 @@ public class MainPage extends AppCompatActivity {
         }
     }
 
-    void subscribeRating(final Handler handler)
+    void subscribe(final Handler handler)
     {
         subscribeThread = new Thread(new Runnable() {
             @Override
@@ -123,27 +165,23 @@ public class MainPage extends AppCompatActivity {
                         Channel channel = connection.createChannel();
                         channel.basicQos(1);
                         AMQP.Queue.DeclareOk q = channel.queueDeclare();
-                        channel.queueBind(q.getQueue(), "amq.fanout", "rating");
+                        channel.queueBind(q.getQueue(), "amq.fanout", "chat");
                         QueueingConsumer consumer = new QueueingConsumer(channel);
-                        boolean autoAck = false;
-                        channel.basicConsume(q.getQueue(), autoAck, consumer);
+                        channel.basicConsume(q.getQueue(), true, consumer);
 
                         // Process deliveries
                         while (true) {
                             QueueingConsumer.Delivery delivery = consumer.nextDelivery();
 
-                            ByteBuffer buf = ByteBuffer.wrap(delivery.getBody());
-                            float message = buf.getFloat();
+                            String message = new String(delivery.getBody());
+                            Log.d("'","[r] " + message);
 
                             Message msg = handler.obtainMessage();
                             Bundle bundle = new Bundle();
 
-                            bundle.putFloat("rating", message);
+                            bundle.putString("msg", message);
                             msg.setData(bundle);
                             handler.sendMessage(msg);
-                            boolean requeue = false;
-                            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), requeue );
-                            Log.d("'", "Consumed message!");
                         }
                     } catch (InterruptedException e) {
                         break;
@@ -161,7 +199,7 @@ public class MainPage extends AppCompatActivity {
         subscribeThread.start();
     }
 
-    public void publishToAMQPRating()
+    public void publishToAMQP()
     {
         publishThread = new Thread(new Runnable() {
             @Override
@@ -173,21 +211,14 @@ public class MainPage extends AppCompatActivity {
                         ch.confirmSelect();
 
                         while (true) {
-                            int quesize = queueRating.size();
-                            Log.d("'", "QueueSize: "+quesize);
-                            Float message = queueRating.takeFirst();
+                            String message = queue.takeFirst();
                             try{
-
-                                //Allocate space for one float (ch.basicPublish only likes ByteArrays)
-                                final ByteBuffer buf = ByteBuffer.allocate(4).putFloat(message);
-
-                                ch.basicPublish("amq.fanout", "rating", null, buf.array());
-                                Log.d("'", "[s]rating " + message);
-
+                                ch.basicPublish("amq.fanout", "chat", null, message.getBytes());
+                                Log.d("'", "[s] " + message);
                                 ch.waitForConfirmsOrDie();
                             } catch (Exception e){
-                                Log.d("'","[f]rating " + message);
-                                queueRating.putFirst(message);
+                                Log.d("'","[f] " + message);
+                                queue.putFirst(message);
                                 throw e;
                             }
                         }
